6d0307b22c546a84dc76b9f13f4400e151133559
“ducngg” <ducng1311@gmail.com>
2023-10-09 04:20:38 -0400

    Add files

---
+++ firstproject/firstapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import date
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+                "Today's date is {}" \
+               "</html>".format(today)
+    return HttpResponse(content=template)
+
+def index(request):
+    template = "<html>" \
+                "This is your first view" \
+               "</html>"
+    return HttpResponse(content=template)
